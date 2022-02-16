73a64b42542efd66eeb3c9bb8dcc4cd894f863fd
Pierre-Antoine Bannier <pierreantoine.bannier@gmail.com>
2022-02-16 13:27:15 +0100

    added tests + benchmarks

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ mod sort;
 mod tests {
     use super::sort::{
         bubble_sort, heap_sort::heap_sort, merge_sort::merge_sort, quick_sort::quick_sort,
+        radix_sort::radix_sort,
     };
     use rand::prelude::*;
     use rand_pcg::Pcg64;
@@ -25,15 +26,18 @@ mod tests {
         let mut arr2 = arr.to_owned();
         let mut arr3 = arr.to_owned();
         let mut arr4 = arr.to_owned();
+        let mut arr5 = arr.to_owned();
 
         bubble_sort(&mut arr);
         merge_sort(&mut arr2);
         heap_sort(&mut arr3);
         quick_sort(&mut arr4);
+        radix_sort(&mut arr5);
 
         assert_eq!(arr, arr2);
         assert_eq!(arr2, arr3);
         assert_eq!(arr3, arr4);
+        assert_eq!(arr4, arr5);
     }
 
     #[bench]
@@ -65,6 +69,14 @@ mod tests {
         let mut arr = get_unsorted_vector(0);
         b.iter(|| {
             quick_sort(&mut arr);
+        })
+    }
+
+    #[bench]
+    fn bench_radix_sort(b: &mut Bencher) {
+        let mut arr = get_unsorted_vector(0);
+        b.iter(|| {
+            radix_sort(&mut arr);
         })
     }
 }
